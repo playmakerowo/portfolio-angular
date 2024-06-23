bd9d63dd46db48ee4a9e2b28ecba21541aeda6ad
jorge villagra <jorgevillagrafredes@gmail.com>
2024-06-23 17:15:47 -0400

    Experiencia

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SkillsComponent } from '../../components/skills/skills.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
+import { ExperienciaComponent } from '../../components/experiencia/experiencia.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SkillsComponent, ProjectsComponent],
+  imports: [SkillsComponent, ProjectsComponent, ExperienciaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
