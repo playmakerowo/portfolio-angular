711938d7ba4e465faa4e920b8457274573a77571
jorge villagra <jorgevillagrafredes@gmail.com>
2024-06-22 22:32:58 -0400

    SCSS global y proyectos

---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'projects',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SkillsComponent } from '../../components/skills/skills.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SkillsComponent],
+  imports: [SkillsComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
